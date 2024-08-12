f62097a25f11aa3be88f866f113dabe998b197e0
rcaleb <calebmadara58@gmail.com>
2024-08-12 11:29:53 +0300

    Define data models in models.go for Groupie Tracker application
    - Create Data struct to aggregate Artist, Relation, Location, and Date models.
    - Implement Artist, Location, Date, and Relation structs with appropriate JSON tags for API integration.

---
+++ models/models.go
@@ -0,0 +1,30 @@
+package models
+
+type Data struct {
+	A Artist
+	R Relation
+	L Location
+	D Date
+}
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Members      []string `json:"members"`
+}
+
+type Location struct {
+	Locations []string `json:"locations"`
+}
+
+type Date struct {
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type Relation struct {
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
