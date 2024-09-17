3b4ff8b4fee482ba4208973d3d33e6497f9dae88
rcaleb <calebmadara58@gmail.com>
2024-09-17 11:24:32 +0300

    feat: Preload artist data and implement search functionality
    - Added data collection and preloading from the `api.CollectData` function.
    - Introduced `SearchIndex` struct in `models` to store searchable data, including artist names, member names, locations, first albums, and creation dates.
    - Implemented data preloading into the search index for faster lookups.
    - Added a new route `/search` with `SearchHandler` to handle artist search functionality.
    - Updated server port from 8080 to 8081 for the new implementation.

--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"learn.zone01kisumu.ke/git/rcaleb/groupie-tracker/api"
 	"learn.zone01kisumu.ke/git/rcaleb/groupie-tracker/handlers"
+	"learn.zone01kisumu.ke/git/rcaleb/groupie-tracker/models"
 )
 
 func main() {
+	// Collect and preload data
+	data, err := api.CollectData()
+	if err != nil {
+		log.Fatalf("Failed to collect data: %v", err)
+	}
+	// Initialize the search index
+	searchIndex := &models.SearchIndex{
+		ArtistName:   make(map[string][]models.IndexedData),
+		MemberName:   make(map[string][]models.IndexedData),
+		LocationName: make(map[string][]models.IndexedData),
+		FirstAlbum:   make(map[string][]models.IndexedData),
+		CreationDate: make(map[int][]models.IndexedData),
+	}
+
+	searchIndex.PreloadData(data)
 
 	http.HandleFunc("/artistInfo", handlers.ArtistInfo)
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/static/", handlers.StaticServer)
-	fmt.Println("http://localhost:8080/")
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/search", searchIndex.SearchHandler)
+	fmt.Println("http://localhost:8081/")
+	http.ListenAndServe(":8081", nil)
 }
